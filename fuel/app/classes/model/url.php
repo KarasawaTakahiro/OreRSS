be04dc272179300aebefc443fd03887a21586f80
Karasawa Takahiro <tkhr.karasawa@gmail.com>
2014-10-09 16:06:07 +0900

    url.php : フィード一覧ページのURLを返すメソッドを追加

--- fuel/app/classes/model/url.php
+++ fuel/app/classes/model/url.php
@@ -1,15 +1,17 @@
 <?php
 
+define('PAGE_FEED', 'orerss/feed');
 
-class Model_Url extends \Model
+
+class Model_Url extends \Model{ 
 {
-
-    public static function gen_continuous_playback_url($mylist_id, $movie_url){
-        return $movie_url."?playlist_type=mylist_playlist&group_id=$mylist_id&mylist_sort=0";
+    public static function gen_continuous_playback_url($mylist_id, $movie_url){ 
+        return $movie_url."?playlist_type=mylist_playlist&group_id=$mylist_id&mylist_sort=0"; 
     }
 
-
+    public static function gen_feed_page_url($feed_id){
+        return PAGE_FEED.'/'.$feed_id;
+    }
 
 }
-
 
