3bfbdaf165d090c4f5a1274a3dff968d3b7735c3
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-01-13 14:02:51 +0900

    fix     アイテムリストのautoMarkの矢印の方向を選択できるようにした

--- fuel/app/classes/controller/orerss.php
+++ fuel/app/classes/controller/orerss.php
@@ -42,6 +42,7 @@ class Controller_Orerss extends Controller_Template
                                  ),
             'items'     => Model_Itemtbl::get_all_unread_itemlist($userid),                     // 未読のitem全てのリスト
             'nickname'  => self::help_nickname(),
+            'direction' => 'up',
         );
 
 
@@ -72,6 +73,7 @@ class Controller_Orerss extends Controller_Template
             'items'     => Model_Url::convert_continuous_playback_url(Model_Itemtbl::get_itemlist_column_from_feed_id_with_watched($feed_id, $userid)),
             'nickname'  => self::help_nickname(),
             'userlist'  => Model_Pull::get_pull_users($feed_id, $userid),
+            'direction' => 'down',
         );
 
         // 404
