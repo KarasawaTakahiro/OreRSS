b2caf492b442b43481271642aadf5470de6c812a
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-01-17 18:50:07 +0900

    fix     $の位置ミス

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -5,7 +5,7 @@ $(function(){
 
 var bind_checkNickname = function(){
     var preString = "";                     // 比較対象文字列
-    ($"#inputNickname").keyup(function(){   // すべてのキー操作にバインド
+    $("#inputNickname").keyup(function(){   // すべてのキー操作にバインド
         var string = $(this).val();         // テキストボックスの値取得
         if(string != preString){            // 文字列比較
             checkNickname();                // 違ったら呼び出し
