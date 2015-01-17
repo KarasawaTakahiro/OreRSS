db900cbc5eb22a9e521e21650542c925c6b629b9
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-01-17 18:42:14 +0900

    add     /signup で呼ぶjsファイル追加

---
+++ public/assets/js/signup.js
@@ -0,0 +1,26 @@
+$(function(){
+    bind_checkNickname();
+    bind_checkPasswd();
+});
+
+var bind_checkNickname = function(){
+    var preString = "";                     // 比較対象文字列
+    ($"#inputNickname").keyup(function(){   // すべてのキー操作にバインド
+        var string = $(this).val();         // テキストボックスの値取得
+        if(string != preString){            // 文字列比較
+            checkNickname();                // 違ったら呼び出し
+            preString = string;             // 文字列入れ替え
+        }
+    });
+};
+
+var bind_checkPasswd = function(){
+};
+
+var checkNickname = function(){
+    console.log("call");
+};
+
+var checkPasswd = function(){
+};
+
