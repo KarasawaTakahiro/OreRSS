581d2d4769df21ed9a7f21753b9b2be505c0d073
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-01-24 08:34:12 +0900

    fix     パスワードチェック関数の呼び出し失敗を修正

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -16,12 +16,13 @@ var bind_checkNickname = function(){
 
 var bind_checkPasswd = function(){
     var preString = "";                     // 比較対象文字列
-    $("#inputNickname").keyup(function(){   // すべてのキー操作にバインド
+    $("#inputPassword").keyup(function(){   // すべてのキー操作にバインド
         var string = $(this).val();         // テキストボックスの値取得
         if(string != preString){            // 文字列比較
-            checkNickname();                // 違ったら呼び出し
+            checkPasswd();                  // 違ったら呼び出し
             preString = string;             // 文字列入れ替え
         }
+        console.log(string);
     });
 };
 
