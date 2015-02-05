3821e57e47644ef65c4d72b8794d0f7d10828453
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-02-05 13:06:57 +0900

    add     フィード情報を取得する際に購読数も取得する

--- fuel/app/classes/model/feedtbl.php
+++ fuel/app/classes/model/feedtbl.php
@@ -152,7 +152,8 @@ class Model_Feedtbl extends \Model
      */
     public static function get_user_pull($userid)
     {
-        $query = \DB::select('feed.id', 'feed.title', 'feed.url')->from('feed')
+        $query = \DB::select('feed.id', 'feed.title', 'feed.url', 'feed.pull_num')
+            ->from('feed')
             ->join('pull')->on('feed.id', '=', 'pull.feed_id')
             ->join('user')->on('user.id', '=', 'pull.user_id')
             ->where('user.id', '=', $userid)
