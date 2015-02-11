47930a6beadc092c87a4e7292e9f22eac87582a4
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-02-11 20:34:00 +0900

    fix     #39 比較する時刻を変えていない問題を解決

--- database/feedupdate.py
+++ database/feedupdate.py
@@ -7,30 +7,39 @@ import time
 
 class FeedUpdate():
     def __init__(self):
-        os.environ["TZ"] = "Asia/Tokyo"
-        self.url = "http://localhost/orerss/updateFeed"
-        self.delay = 60
-        self.delayminute = 10
-        self.headers = {"pragma":"no-cache"}
-        self.outfile = "./log_update.txt"
+        os.environ["TZ"] = "Asia/Tokyo"                     # タイムゾーンの設定
+        self.url = "http://localhost/orerss/updateFeed"     # フィード更新API
+        self.headers = {"pragma":"no-cache"}                # HTTPヘッダ
+        self.delay = 60                                     # 更新時間のチェックの間隔[sec]
+        self.delayminute = 30                               # feedの更新間隔[min]
+        self.outfile = "./log_update.txt"                   # ログファイル名
 
     def update(self):
+        """
+            更新処理呼び出し
+        """
         self._update()
 
     def _update(self):
-        minute = int(time.strftime("%M"))
+        """
+            メイン処理
+        """
         while True:
-            if minute % self.delayminute == 0:
-                req = urllib2.Request(self.url, "\r\n", self.headers)
-                res = urllib2.urlopen(req)
-                self.log(res.read())
-            time.sleep(self.delay)
+            minute = int(time.strftime("%M"))               # 現在の分を取得
+            if minute % self.delayminute == 0:              # 更新タイミング?
+                req = urllib2.Request(self.url, "\r\n", self.headers)   # リクエスト作成
+                res = urllib2.urlopen(req)                              # APIにリクエスト
+                self.log(res.read())                                    # 結果をログに記録
+            time.sleep(self.delay)                          # スリープ
 
     def log(self, string):
-        f = open(self.outfile, "a")
-        t = time.strftime("%Y/%m/%d %H:%M:%S %Z")
-        f.write("[%s] : udpate (%s)\n" % (t, string))
-        f.close()
+        """
+            ロギング
+        """
+        f = open(self.outfile, "a")                         # 追記でファイルオープン
+        t = time.strftime("%Y/%m/%d %H:%M:%S %Z")           # YYYY/mm/dd HH:MM:SS TZ
+        f.write("[%s] : udpate (%s)\n" % (t, string))       # 記録
+        f.close()                                           # ファイルクローズ
 
 if __name__ == "__main__":
     feedupdate = FeedUpdate()
