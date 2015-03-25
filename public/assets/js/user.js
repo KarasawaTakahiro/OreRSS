b314d594ea6e39aace3a37b0ecc6d16684e5aa0e
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2015-03-25 19:50:10 +0900

    add     #74 説明文のトグル

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,6 +1,9 @@
 $(function(){
     bind_smartPush();
+    bind_description();
 });
+
+var description_default_height = 0;
 
 /*
  * ユーザページのマイリストから購読する
@@ -22,12 +25,33 @@ var smartPush = function(mylistUrl){
 };
 
 /*
+ * 説明文の範囲をトグル
+ */
+var toggle_description = function(obj){
+    desc = obj.parents(".list-item").find(".description")[0];
+    if(description_default_height < desc.scrollHeight && desc.scrollHeight != desc.offsetHeight){
+        // 広げる
+        $(desc).animate({"height":desc.scrollHeight});
+    }else{
+        // 元に戻す
+        $(desc).animate({"height":description_default_height});
+    }
+};
+
+/*
  * バインド
  */
 var bind_smartPush = function(){
     $(".smart-push").click(function(){
-        console.log($(this).attr("href"));
         smartPush($(this).attr("href"));
         return false;                   // ページ遷移無効
+    });
+};
+
+var bind_description = function(){
+    description_default_height = $(".list-item .description").height();
+    console.log(description_default_height);
+    $(".info button").click(function(){
+        toggle_description($(this));
     });
 };
